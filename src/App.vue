162c78c35b339af43f4babbf7746bc464c8c760e
Elena <kunstvanelena@gmail.com>
2024-04-25 17:12:09 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <div id="app">
+    <div id="nav">
+      <div class="logo">
+        <img src="" alt="" />
+      </div>
+      <div class="menu">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/projects">Projects</router-link>
+        <router-link to="/contact">Contact</router-link>
+      </div>
+    </div>
+    <RouterView />
+  </div>
+  <main>
+    <h1 class="text-3xl font-bold underline">GAHYUN</h1>
+    <h2>Frontend Developer</h2>
+    <h2>UX/UI Designer</h2>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="projects">
+    <h1>This is a projects page</h1>
+  </div>
+</template>
+
+<style></style>
